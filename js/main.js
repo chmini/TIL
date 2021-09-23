e8abb82dd0cfedf682ce9805dd4932994300371c
chmini <beelzebule@naver.com>
2021-09-23 21:23:05 +0900

    seperate css

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 const menuBtn = document.querySelector(".menu-btn");
 
-const header = document.querySelector("header");
-const main = document.querySelector("main");
-
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
 });
